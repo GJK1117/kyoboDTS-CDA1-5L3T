746c1290a7d7b7785dd654afa7fc244bab4d618f
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-11-13 16:28:19 +0900

    feat: 홈페이지 도서 정보 게시 API 작성
    - 홈페이지에 개시될 데이터를 보여주는 API 작성
    - 일반도서는 랜덤하게 20개 가져오는 로직
    - 연재소설은 요일별로 20개 보여주는 로직

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -4,6 +4,7 @@ from app.api.general_book_search import router as gbs_router
 from app.api.serial_novel_read import router as snr_router
 from app.api.serial_novel_search import router as sns_router
 from app.api.search_ebooks import router as sb_router
+from app.api.list_ebook import router as home_router
 
 api_router = APIRouter()
 
@@ -12,3 +13,4 @@ api_router.include_router(gbs_router, prefix="/general_book", tags=["general_boo
 api_router.include_router(snr_router, prefix="/serial_novel", tags=["serial_novel"])
 api_router.include_router(sns_router, prefix="/serial_novel", tags=["serial_novel"])
 api_router.include_router(sb_router, prefix="/books", tags=["allbooks"])
+home_router.include_router(sb_router, prefix="/books", tags=["allbooks"])
