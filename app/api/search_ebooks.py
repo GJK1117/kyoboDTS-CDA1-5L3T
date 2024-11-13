982ca0c31c7b8c23c8f9982f1a0c93d88866184a
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-11-13 16:25:24 +0900

    refactor: 검색 API 구분
    - 일반도서와 연재소설 검색 로직을 분리
    - 이에 따른 type 경로 매개변수 삭제 및 사용하는 db schema class 구분

--- app/api/search_ebooks.py
+++ app/api/search_ebooks.py
@@ -2,44 +2,47 @@ from typing import List, Optional
 from fastapi import APIRouter, HTTPException, Depends
 from sqlmodel import select, Session
 from app.core.rds_config import get_read_replica_engine
-from app.schema.mysql_schema import Ebook, SerialBook
+from app.schema.mysql_schema import Generalbook, SerialBook
 
 router = APIRouter()
 
-# 검색어를 경로 매개변수로 받아 검색하는 엔드포인트
-@router.get("/search/{q}/{type}/{last_id}", response_model=List[Ebook])
-def search_books(
+# 10개씩 일반 도서 검색어의 결과를 보여주는 API
+@router.get("/search/gb/{q}/{last_id}", response_model=List[Generalbook])
+def search_general_books(
     q: str,
-    type: int,
-    last_id: Optional[int],  # 마지막 ID를 경로 매개변수로 사용
+    last_id: Optional[int] = None,  # 마지막 ID를 경로 매개변수로 사용
     session: Session = Depends(get_read_replica_engine)
 ):
-    # 쿼리를 저장할 변수 초기화
-    query = None
+    # Ebook 테이블에서 검색
+    query = select(Generalbook).where(Generalbook.title.like(f"%{q}%"))
+    if last_id is not None:
+        query = query.where(Generalbook.id > last_id)
+    query = query.order_by(Generalbook.id.asc()).limit(10)
 
-    # type 값에 따라 다른 테이블에서 검색
-    if type == 0:
-        # Ebook 테이블에서 검색
-        query = select(Ebook).where(Ebook.title.like(f"%{q}%"))
-        if last_id is not None:
-            query = query.where(Ebook.id > last_id)
-        query = query.order_by(Ebook.id.asc()).limit(10)
-        
-    elif type == 1:
-        # SerialBook 테이블에서 검색
-        query = select(SerialBook).where(SerialBook.title.like(f"%{q}%"))
-        if last_id is not None:
-            query = query.where(SerialBook.id > last_id)
-        query = query.order_by(SerialBook.id.asc()).limit(10)
-
-    # query가 설정되지 않았으면 잘못된 type 값이 전달된 것으로 처리
-    if query is None:
-        raise HTTPException(status_code=400, detail="Invalid type parameter. Use 0 for Ebook or 1 for Series.")
-
-    # 쿼리 실행
     results = session.exec(query).all()
     
     if not results:
-        raise HTTPException(status_code=404, detail="No more books found.")
+        raise HTTPException(status_code=404, detail="No more General books found.")
+    
+    return results
+
+
+# 10개씩 연재 소설 검색어의 결과를 보여주는 API
+@router.get("/search/sn/{q}/{last_id}", response_model=List[SerialBook])
+def search_serial_books(
+    q: str,
+    last_id: Optional[int] = None,  # 마지막 ID를 경로 매개변수로 사용
+    session: Session = Depends(get_read_replica_engine)
+):
+    # SerialBook 테이블에서 검색
+    query = select(SerialBook).where(SerialBook.title.like(f"%{q}%"))
+    if last_id is not None:
+        query = query.where(SerialBook.id > last_id)
+    query = query.order_by(SerialBook.id.asc()).limit(10)
+
+    results = session.exec(query).all()
+    
+    if not results:
+        raise HTTPException(status_code=404, detail="No more serial books found.")
     
     return results
