3740cdd8dc3a34a782bbf9670d2df9ed1fa30864
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-11-13 16:23:30 +0900

    fix: readreplica, primarydb 접근 구분
    - 두 db의 접근 주소가 다르므로 다르게 정의하도록 코드 수정 및 추가

--- app/core/rds_config.py
+++ app/core/rds_config.py
@@ -8,7 +8,7 @@ READ_REPLICA_DATABASE_URL = os.getenv("READ_REPLICA_DATABASE_URL", "mysql+pymysq
 # 연결 풀 설정
 # 요청마다 연결을 구성하는 것은 시간이 오래 걸릴 것으로 판단, 계속된 연결을 유지하는 연결 풀을 생성
 read_replica_engine = create_engine(
-    READ_REPLICA_DATABASE_URL,
+    f"mysql+pymysql://{READ_REPLICA_DATABASE_URL}",
     pool_size=10,          # 최대 10개의 연결을 유지
     max_overflow=20,       # 최대 풀 크기 초과 시 20개의 추가 연결 허용
     pool_timeout=30,       # 풀에 연결이 없을 때 최대 대기 시간(초)
@@ -24,7 +24,7 @@ def get_read_replica_engine():
 DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://username:password@host/dbname")
 
 engine = create_engine(
-    DATABASE_URL,
+    f"mysql+pymysql://{DATABASE_URL}",
     pool_size=10,          # 최대 10개의 연결을 유지
     max_overflow=20,       # 최대 풀 크기 초과 시 20개의 추가 연결 허용
     pool_timeout=30,       # 풀에 연결이 없을 때 최대 대기 시간(초)
